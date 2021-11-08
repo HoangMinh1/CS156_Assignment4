2cbc8693711ce1ad3541e13e6e095572a977cc4a
Brooke <brooke.dalton@sjsu.edu>
2021-11-07 16:07:33 -0800

    First and second step of assignment 4B

---
+++ assignment_4B.py
@@ -0,0 +1,55 @@
+import sys
+import glob
+import string
+
+# Step 1: Create a command argument that allows passing path of the directory as input
+# example: python3 filename <inputPath>
+inputPath = sys.argv[1]
+
+posPath = inputPath + "/pos/"
+negPath = inputPath + "/neg/"
+
+posFiles = glob.glob(posPath + "*.txt") # pattern matching files with .txt 
+negFiles = glob.glob(negPath + "*.txt")
+
+
+# Step 2: Automatically read the whole file and convert them into a form that
+# can be trainable (bag of words)
+# ex: ["hello": 1, "my": 2 ...] where 1 & 2 is frequency of the word "hello" & "my"
+bagOfWords = []
+
+for filePath in posFiles:
+    with open(filePath, 'r') as file:
+        content = file.read().split()
+
+        freq = {}
+        for word in content:
+            # to lower case
+            lower_content = word.lower()
+            # remove punctuation
+            no_punct = lower_content.translate(str.maketrans('','',string.punctuation))
+            # remove '\n' from string
+            no_newLine = no_punct.replace('\n', '')
+            # count frequency of words
+            count = freq.setdefault(no_newLine, 0)
+            count += 1
+            freq[no_newLine] += 1
+
+        # convert dictionary to list
+        word_list = list(freq.items())
+        # append frequency count and word to bagOfWords
+        for key, value in word_list:
+            bagOfWords.append([key, value])
+    file.close()
+
+print(bagOfWords)
+
+# Step 3: Calculate features which has highest P(feature | pos) and P(feature | neg)
+# Print out top 5 (Assignment 4B)
+
+# Step 4: Selecting features
+# calculate the usefuleness of each word using the formula on slide 18 p11-12
+# Select the data which have the highest features
+
+
+# Step 5: Write a Bayes classification
